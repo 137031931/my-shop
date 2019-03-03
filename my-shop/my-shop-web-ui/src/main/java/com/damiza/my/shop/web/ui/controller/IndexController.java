1c7c73e235756f1f106b8f92c4ad0592f567cddf
137031931 <137031931@qq.com>
2019-03-03 23:56:20 +0800

    indexcontroller

--- my-shop/my-shop-web-ui/src/main/java/com/damiza/my/shop/web/ui/controller/IndexController.java
+++ my-shop/my-shop-web-ui/src/main/java/com/damiza/my/shop/web/ui/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.damiza.my.shop.web.ui.controller;
 
+import com.damiza.my.shop.commons.utils.HttpClientUtils;
+import com.damiza.my.shop.commons.utils.MapperUtils;
+import com.damiza.my.shop.web.ui.dto.TbContent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +16,13 @@ public class IndexController {
      */
     @RequestMapping(value = {"","index"},method = RequestMethod.GET)
     public String index(){
+        String result = HttpClientUtils.doGet("http://localhost:8081/api/v1/contents/89");
+        try {
+            MapperUtils.json2list(result, TbContent.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(result);
         return "index";
     }
 }
